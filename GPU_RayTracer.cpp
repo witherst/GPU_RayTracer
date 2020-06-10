30ec5b719ccbe7634b9f0e6374ae3a54bba40671
clay.withers@gmail.com <clay.withers@gmail.com>
2020-06-10 15:48:15 -0700

    Got rid of previous hit_sphere change (from youtube video) as it was EXTRAORDINARILY slower. Like, orders of magnitudes slower. I understand the old one now. Changed hit_sphere to visualize the Normals of the sphere.

--- GPU_RayTracer.cpp
+++ GPU_RayTracer.cpp
@@ -1,6 +1,12 @@
 // GPU_RayTracer.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+// OMP for performance tests
+#pragma omp
+#include <omp.h>
+#define PERF 0
+
+// Regular include files
 #include <iostream>
 #include <fstream>
 #include <GL/glut.h>
@@ -31,14 +37,14 @@ void display(void)
 
 int main(int argc, char** argv)
 {
-    int i, j;
+    // Setup glut args
     glutInit(&argc, argv);
     glutInitWindowSize(SIZEX, SIZEY);
     glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
     glutCreateWindow("glDrawPixels example");
-
     glutDisplayFunc(display);
-
+    
+    // Pixels to be drawn to opengl display window
     pixels = (unsigned char*)malloc(SIZEX * SIZEY * 3);
     pptr = pixels;
 
@@ -50,32 +56,50 @@ int main(int argc, char** argv)
 
     // Define spheres to be drawn
     Sphere *spheres = new Sphere[1];
-
     spheres[0].setP(0, 0, -1);
     spheres[0].setRadius(.5);
     spheres[0].setColor(1, 0, 0);
 
-//    for (j = SIZEY - 1; j >= 0; j--) {    
-    for (j = 0; j < SIZEY; j++) {               // OpenGL flips the Y direction. So need to do it this way for OpenGL display.
-        for (i = 0; i < SIZEX; i++) {
-            float u = float(i) / float(SIZEX);
-            float v = float(j) / float(SIZEY);
+    // OpenMP performance vars
+    double startTime, endTime;
+    int numloops = 1;
+    if (PERF) { 
+        startTime = omp_get_wtime();
+        numloops = 100;
+    } 
 
-            ray r(origin, lower_left_corner + u * horizontal + v * vertical);
+    for (int k = 0; k < numloops; k++) {
+        // Reset pointer to pixel array (only useful if we're doing performance evaluations)
+        pptr = pixels;
 
-            vec3 col = color(r, spheres);
-            int ir = int(255.99 * col[0]);
-            int ig = int(255.99 * col[1]);
-            int ib = int(255.99 * col[2]);
+//      for (j = SIZEY - 1; j >= 0; j--) {    
+        for (int j = 0; j < SIZEY; j++) {               // OpenGL flips the Y direction. So need to do it this way for OpenGL display.
+            for (int i = 0; i < SIZEX; i++) {
+                float u = float(i) / float(SIZEX);
+                float v = float(j) / float(SIZEY);
 
-            *pptr++ = ir;    /* red */
-            *pptr++ = ig;    /* green */
-            *pptr++ = ib;    /* blue */
+                ray r(origin, lower_left_corner + u * horizontal + v * vertical);
+
+                vec3 col = color(r, spheres);
+                int ir = int(255.99 * col[0]);
+                int ig = int(255.99 * col[1]);
+                int ib = int(255.99 * col[2]);
+
+                *pptr++ = ir;    /* red */
+                *pptr++ = ig;    /* green */
+                *pptr++ = ib;    /* blue */
+            }
         }
     }
+    if (PERF) {
+        endTime = omp_get_wtime();
+//        cout << "Performance: " << (double)numloops / (endTime - startTime) / 1000000.;
+        cout << "Performance: " << (endTime - startTime);
+    }
+
 
     // Display final results
-    glutMainLoop();
+    glutMainLoop(); 
     return 0;
 }
 
--- ray.cpp
+++ ray.cpp
@@ -5,51 +5,32 @@
 using std::cout;
 using std::endl;
 
-//bool hit_sphere(const ray& r, const Sphere* s) {
-//	vec3 oc = r.origin() - s->getP();
-//	float a = dot(r.direction(), r.direction());
-//	float b = 2.0 * dot(oc, r.direction());
-//	float c = dot(oc, oc) - s->getRadius() * s->getRadius();
-//	float discriminant = b * b - 4 * a * c;
-//	
-//	return(discriminant > 0);
-//}
+float hit_sphere(const ray& r, const Sphere* s) {
+	vec3 oc = r.origin() - s->getP();
+	float a = dot(r.direction(), r.direction());
+	float b = 2.0 * dot(oc, r.direction());
+	float c = dot(oc, oc) - s->getRadius() * s->getRadius();
+	float discriminant = b * b - 4 * a * c;
 
-/*
-Explanation of the below sphere intersection equation: 
-	https://www.youtube.com/watch?v=HFPlKQGChpE
-*/
-bool hit_sphere(const ray& r, const Sphere* s) {
-	float t = dot((s->getP() - r.origin()), r.direction());
-
-	// Check negative t, if so, ray missed the sphere
-	if (t < 0) {
-		return false;
-	}
-	vec3 p = s->getP() - r.point_at_parameter(t);
-	float y = p.length();
-
-	// Check if y is bigger than radius, ray missed
-	if (y > s->getRadius()) {
-		return false;
-	}
-
-	float x = sqrtf((s->getRadius() * s->getRadius()) - (y * y));
-
-	float t1 = t - x;	// Intersection 1
-	float t2 = t + x;	// Intersection 2
-	return (t1 > 0 && t2 > 0);
+	if (discriminant < 0) { return -1; }
+	else {
+		// Return smallest t value
+		return ((-b - sqrtf(discriminant)) / (2 * a));
+	}	
 }
 
 vec3 color(const ray& r, const Sphere* s) {
 
 	// Check for sphere intersection and return color of the sphere
-	if (hit_sphere(r,s)) {
-		return s[0].getColor();
+	float t = hit_sphere(r, s);
+
+	if (t > 0.0) {
+		vec3 N = r.point_at_parameter(t) - s->getP();	
+		return .5 * vec3(N.x() + 1, N.y() + 1, N.z() + 1);
 	}
 
 	// Color background the faded blue to white
 	vec3 unit_dir = unit_vector(r.direction());
-	float t = .5 * (unit_dir.y() + 1.0);
+	t = .5 * (unit_dir.y() + 1.0);
 	return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
 }
